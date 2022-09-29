8ddd2ad7f2209a3fcb231ba3018d062b6fb71df6
chentsulin <chentsulin@gmail.com>
2022-09-29 16:15:53 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    browsersListForSwc: true,
+    legacyBrowsers: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
